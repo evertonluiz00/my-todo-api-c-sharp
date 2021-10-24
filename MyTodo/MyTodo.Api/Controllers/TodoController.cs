a9043954b2f95b80cd308c449fdd655ea5b3b839
Everton Christmann <evertonluiz00@hotmail.com>
2021-10-24 14:53:56 -0300

    Initial commit

---
+++ MyTodo/MyTodo.Api/Controllers/TodoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTodo.Api
+{
+    [ApiController]
+    [Route(template:"v1")]
+    public class TodoController : ControllerBase
+    {
+        [HttpGet]
+        [Route(template:"todos")]
+        public List<Todo> Get()
+        {
+            return new List<Todo>();
+        }
+    }
+}
---
+++ MyTodo/MyTodo.Api/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTodo.Api
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(connectionString:"DataSource=app.db;Cache=Shared");
+        }
+    }
+}
